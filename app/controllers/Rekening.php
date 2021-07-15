cbd71d94a97e1802c0b639af9457311fe26b6d70
Ade Kurniawan <indieplugins@gmail.com>
2021-07-16 01:45:26 +0700

    Add rekening

---
+++ app/controllers/Rekening.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace App\Controller;
+
+class Rekening extends \App\Core\Controller {
+
+  public function index() {
+
+    /* Admin only */
+    if ($_SESSION['role'] != 'admin') {
+      header('HTTP/1.0 403 Forbidden');
+      die();
+    }
+
+    $rekening = $this->model('Rekening');
+
+    $data = array();
+    $data['doc_title'] = 'Daftar Rekening';
+
+    $page = isset($_GET['page']) ? (int)$_GET['page'] : '';
+    $data['rekening'] = $rekening->getPaginatedData(\App\Core\Paging::getLimit($page));
+    $data['total_data'] = \App\Core\Paging::getTotalData($rekening->getSql());
+    $data['paging'] = \App\Core\Paging::getLinks($data['total_data'], $page);
+
+    \App\Core\Sidebar::setActiveIcon('rekening')::setActiveLink('rekening');
+    $this->show('rekening/daftar', $data);
+    
+  }
+
+  public function form($id = 0) {
+
+    /* Admin only */
+    if ($_SESSION['role'] != 'admin') {
+      header('HTTP/1.0 403 Forbidden');
+      die();
+    }
+
+    $rekening = $this->model('Rekening');
+
+    $data = array();
+    $data['id_rekening'] = $id;
+    $data['error'] = array();
+    $data['default'] = array(
+      'bank' => '',
+      'kantor_cabang' => '',
+      'nomor_rekening' => '',
+      'nama_pemilik_rekening' => '',
+      'jenis_rekening' => ''
+    );
+
+    $currentData = array();
+    if ($id > 0) {
+      $currentData = $rekening->getDataById($id);
+      if ($currentData) {
+        $data['default']['bank'] = $currentData['bank'];
+        $data['default']['kantor_cabang'] = $currentData['kantor_cabang'];
+        $data['default']['nomor_rekening'] = $currentData['nomor_rekening'];
+        $data['default']['nama_pemilik_rekening'] = $currentData['nama_pemilik_rekening'];
+        $data['default']['jenis_rekening'] = $currentData['jenis_rekening'];
+      }
+      else {
+        header('HTTP/1.0 403 Forbidden');
+        die();
+      }
+    }
+
+    if (isset($_POST['submit'])) {
+      $set = array();
+      $set['bank'] = isset($_POST['bank']) ? trim($_POST['bank']) : '';
+      $set['kantor_cabang'] = isset($_POST['kantor_cabang']) ? trim($_POST['kantor_cabang']) : '';
+      $set['nomor_rekening'] = isset($_POST['nomor_rekening']) ? trim($_POST['nomor_rekening']) : '';
+      $set['nama_pemilik_rekening'] = isset($_POST['nama_pemilik_rekening']) ? trim($_POST['nama_pemilik_rekening']) : '';
+      $set['jenis_rekening'] = isset($_POST['jenis_rekening']) ? trim($_POST['jenis_rekening']) : '';
+      
+      if ($set['bank'] == '') {
+        $data['error']['bank'] = 'Bank harus diisi';
+      }
+
+      if ($set['kantor_cabang'] == '') {
+        $data['error']['kantor_cabang'] = 'Kantor cabang harus diisi';
+      }
+
+      if ($set['nomor_rekening'] == '') {
+        $data['error']['nomor_rekening'] = 'Nomor rekening harus diisi';
+      }
+
+      if ($set['nama_pemilik_rekening'] == '') {
+        $data['error']['nama_pemilik_rekening'] = 'Nama pemilik rekening harus diisi';
+      }
+
+      if ($set['jenis_rekening'] == '') {
+        $data['error']['jenis_rekening'] = 'Jenis rekening harus diisi';
+      }
+
+      $data['default']['bank'] = $set['bank'];
+      $data['default']['kantor_cabang'] = $set['kantor_cabang'];
+      $data['default']['nomor_rekening'] = $set['nomor_rekening'];
+      $data['default']['nama_pemilik_rekening'] = $set['nama_pemilik_rekening'];
+      $data['default']['jenis_rekening'] = $set['jenis_rekening'];
+      
+      if (count($data['error']) < 1) {
+        if ($id > 0) {
+          $dataIsEdited = $rekening->edit($id, $set);
+          if ($dataIsEdited) {
+            \App\Core\Flasher::set('rekening-form', '<p><strong>Rekening dengan jenis `' . $set['jenis_rekening'] . '` berhasil diedit</strong>.</p>', 'success');
+            header('location:' . BASE_URL . '/rekening/form/' . $id);
+            die();  
+          }
+          else {
+            $data['error']['header'] = $rekening->getErrorCode() == '23000' ? 'Jenis rekening <strong>`' . $set['jenis_rekening'] . '`</strong> telah dipakai' : $rekening->getErrorInfo();
+            $data['error']['jenis_rekening'] = 'Jenis rekening telah dipakai';
+          }
+        }
+        else {
+          $dataIsAdded = $rekening->add($set);
+          if ($dataIsAdded) {
+            $lastInsertId = $rekening->getLastInsertId();
+            \App\Core\Flasher::set('rekening-form', '<p><strong>Rekening baru dengan jenis `' . $set['jenis_rekening'] . '` berhasil disimpan</strong>. <br><a href="' . BASE_URL . '/rekening/form/' . $lastInsertId . '">Edit rekening ' . $set['jenis_rekening'] . '</a> atau buat baru lagi dengan menggunakan form di bawah.</p>', 'success');
+            header('location:' . BASE_URL . '/rekening/form');
+            die();
+          }
+          else {
+            $data['error']['header'] = $rekening->getErrorCode() == '23000' ? 'Jenis rekening <strong>`' . $set['jenis_rekening'] . '`</strong> telah dipakai' : $rekening->getErrorInfo();
+            $data['error']['jenis_rekening'] = 'Jenis rekening telah dipakai';
+          }
+        }
+      }
+    }
+
+    $data['title'] = $id > 0 ? 'Edit Rekening' : 'Tambah Rekening';
+    $data['button_label'] = $id > 0 ? 'Edit Data' : 'Tambahkan Data';
+
+    \App\Core\Sidebar::setActiveIcon('rekening')::setActiveLink('rekening');
+    $this->show('rekening/form', $data);
+  }
+
+  public function hapus($id = 0) {
+
+    /* Admin only */
+    if ($_SESSION['role'] != 'admin') {
+      header('HTTP/1.0 403 Forbidden');
+      die();
+    }
+
+    $id = (int) $id;
+    if ($id < 1) {
+      header('location:' . BASE_URL . '/rekening');
+      die();
+    }
+
+    $rekening = $this->model('Rekening');
+    $data = $rekening->getDataById($id);
+    $data['doc_title'] = 'Hapus Rekening';
+    
+    if (isset($_POST['hapus']) && $data['total_jurnal_umum'] < 1) {
+      $rekening->delete($id);
+      \App\Core\Flasher::set('rekening-daftar', '<p><strong>Rekening dengan jenis `' . $data['jenis_rekening'] . '` berhasil dihapus</strong>', 'success');
+      header('location:' . BASE_URL . '/rekening');
+      die();
+    }
+
+    \App\Core\Sidebar::setActiveIcon('rekening')::setActiveLink('rekening');
+    $this->show('rekening/hapus', $data);
+
+  }
+
+}
+
+?>
---
+++ app/views/rekening/daftar.php
@@ -0,0 +1,46 @@
+<div class="heading">
+  <h1>Daftar Rekening</h1>
+  <div class="actions">
+    <a href="<?php echo BASE_URL;?>/rekening/form" class="button secondary">+ Buat rekening baru</a>
+  </div>
+</div>
+
+<!-- Flasher -->
+<?php \App\Core\Flasher::show('rekening-daftar'); ?>
+
+<table class="resp">
+  <thead>
+    <tr>
+      <th width="0%">No</th>
+      <th width="0%">Bank</th>
+      <th width="0%">Kantor cabang</th>
+      <th width="0%">Nomor rekening</th>
+      <th width="0%">Nama pemilik rekening</th>
+      <th width="0%">Jenis rekening</th>
+      <th width="0%"></th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php $i = 0;?>
+    <?php foreach ($data['rekening'] as $k => $v): ?>
+      <tr>
+        <td data-label="No"><?php echo ++$i; ?></td>
+        <td data-label="Bank"><?php echo $v['bank']; ?></td>
+        <td data-label="Kantor cabang"><?php echo $v['kantor_cabang']; ?></td>
+        <td data-label="Nomor rekening"><?php echo $v['nomor_rekening']; ?></td>
+        <td data-label="Nama pemilik cabang"><?php echo $v['nama_pemilik_rekening']; ?></td>
+        <td data-label="Jenis rekening"><?php echo $v['jenis_rekening']; ?></td>
+        <td>
+          <div class="actions">
+            <a href="<?php echo BASE_URL . '/rekening/form/' . $v['id_rekening'];?>"><svg><use xlink:href="<?php echo BASE_URL; ?>/assets/images/sprite.svg#edit"></use></svg></a>
+            <?php if ($v['total_jurnal_umum'] < 1): ?>
+              <a href="<?php echo BASE_URL . '/rekening/hapus/' . $v['id_rekening'];?>" class="dangerous"><svg><use xlink:href="<?php echo BASE_URL; ?>/assets/images/sprite.svg#delete"></use></svg></a>
+            <?php else: ?>
+              <span class="unclickable"><svg><use xlink:href="<?php echo BASE_URL; ?>/assets/images/sprite.svg#delete"></use></svg></span>
+            <?php endif; ?>
+          </div>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
