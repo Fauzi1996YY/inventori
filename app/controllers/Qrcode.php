74df1eafa80d6604c458aab2cdc870e60e72661a
Ade Kurniawan <indieplugins@gmail.com>
2021-09-16 01:03:28 +0700

    Add qr-code

--- app/controllers/Qrcode.php
+++ app/controllers/Qrcode.php
@@ -7,10 +7,22 @@ class Qrcode extends \App\Core\Controller {
   public function index($id = '') {
 
     $barang = $this->model('Barang');
+    $data = $barang->getDataById($id);
+    $item = array();
+    
+    if (!$data) {
+      die('ID barang tidak valid');
+    }
 
+    $item['url'] = BASE_URL . '/barang/form/' . $data['id_barang'];
+    $item['kode'] = $data['kode'];
+    $item['nama'] = $data['nama'];
+    $item['tahun'] = $data['tahun_pembuatan'];
+    $item['brand'] = $data['brand'];
+    // die(json_encode($item));
     include_once('app/vendor/phpqrcode/qrlib.php');
 
-    \QRcode::png($id);
+    \QRcode::png(json_encode($item), false, QR_ECLEVEL_L, 14);
   }
 
 }
